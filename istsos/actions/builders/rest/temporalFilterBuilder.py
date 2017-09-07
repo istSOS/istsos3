1539421c832853693b082d76fe6a760ea4e09bde
Mirko Cardoso <cardoso.mirko@gmail.com>
2017-09-07 11:42:00 +0200

    Fix observation rest api

--- istsos/actions/builders/rest/temporalFilterBuilder.py
+++ istsos/actions/builders/rest/temporalFilterBuilder.py
@@ -4,7 +4,6 @@
 # Version: v3.0.0
 
 import asyncio
-import uuid
 from istsos.entity.filters.temporalFilter import TemporalFilter
 from istsos.actions.builders.temporalFilterBuilder import TemporalFilterBuilder
 
@@ -16,19 +15,19 @@ class TemporalFilterBuilder(TemporalFilterBuilder):
     def process(self, request):
         """ @todo docstring
         """
-        if request['method'] == 'GET':
-            temporalFilter = request.get_parameter('temporalFilter')
-            if temporalFilter is not None:
-                tmpl = TemporalFilter.get_template()
-                tmpl['temporal']['reference'] = 'om:phenomenonTime'
-                if "/" in temporalFilter:
-                    tmpl['temporal'][
-                        'period'] = temporalFilter.split("/")
-                    tmpl['temporal'][
-                        'fes'] = 'during'
-                else:
-                    tmpl['temporal'][
-                        'instant'] = temporalFilter
-                    tmpl['temporal'][
-                        'fes'] = 'equals'
-                request.set_filter(TemporalFilter(json_source=tmpl))
+
+        temporal_filter = request['body']['params'].get("temporalFilter", None)
+        if temporal_filter is not None:
+            tmpl = TemporalFilter.get_template()
+            tmpl['temporal']['reference'] = 'om:phenomenonTime'
+            if "/" in temporal_filter:
+                tmpl['temporal'][
+                    'period'] = temporal_filter.split("/")
+                tmpl['temporal'][
+                    'fes'] = 'during'
+            else:
+                tmpl['temporal'][
+                    'instant'] = temporal_filter
+                tmpl['temporal'][
+                    'fes'] = 'equals'
+            request.set_filter(TemporalFilter(json_source=tmpl))
